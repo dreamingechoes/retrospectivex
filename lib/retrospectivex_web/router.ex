f72fb4c44b1bace9d3f040c88fde2e499aef21c7
Ivan Gonzalez <hi@dreamingecho.es>
2018-06-27 04:42:41 +0200

    Add new pipelines and routes in router.ex

--- lib/retrospectivex_web/router.ex
+++ lib/retrospectivex_web/router.ex
@@ -2,25 +2,51 @@ defmodule RetrospectivexWeb.Router do
   use RetrospectivexWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
+  pipeline :browser_auth do
+    plug(RetrospectivexWeb.Guardian.AuthPipeline)
+    plug(RetrospectivexWeb.Plug.CurrentAdmin)
+  end
+
+  pipeline :browser_ensure_auth do
+    plug(Guardian.Plug.EnsureAuthenticated)
+    plug(RetrospectivexWeb.Plug.CurrentAdmin)
+  end
+
+  pipeline :admin_layout do
+    plug(:put_layout, {RetrospectivexWeb.Admin.LayoutView, :admin})
   end
 
   scope "/", RetrospectivexWeb do
-    pipe_through :browser # Use the default browser stack
+    # Application unauthenticated scope
+    scope "/" do
+      pipe_through([:browser, :browser_auth])
 
-    get "/", PageController, :index
+      get("/", PageController, :index)
+
+      resources("/session", SessionController, only: [:new, :create])
+
+      # Log out resource
+      get("/logout", SessionController, :delete)
+    end
+
+    # Admin scope
+    scope "/admin", Admin, as: :admin do
+      pipe_through([
+        :browser,
+        :browser_auth,
+        :browser_ensure_auth,
+        :admin_layout
+      ])
+
+      resources("/", DashboardController, only: [:index])
+      resources("/administrators", AdministratorController)
+    end
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", RetrospectivexWeb do
-  #   pipe_through :api
-  # end
 end
