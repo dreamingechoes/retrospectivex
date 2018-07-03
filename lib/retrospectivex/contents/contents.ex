f40d4f2c6b69cbc2f702dfe334331223240d9d78
Ivan Gonzalez <hi@dreamingecho.es>
2018-07-03 22:39:02 +0200

    Add order to defdelegate definitions in contents context

--- lib/retrospectivex/contents/contents.ex
+++ lib/retrospectivex/contents/contents.ex
@@ -6,11 +6,11 @@ defmodule Retrospectivex.Contents do
   alias Retrospectivex.Contents.Managers.Page, as: PageManager
 
   # Page API
-  defdelegate list_pages, to: PageManager
-  defdelegate get_page!(id), to: PageManager
-  defdelegate get_page_by_slug!(slug), to: PageManager
-  defdelegate create_page(attrs), to: PageManager
-  defdelegate update_page(page, attrs), to: PageManager
-  defdelegate delete_page(page), to: PageManager
   defdelegate change_page(page), to: PageManager
+  defdelegate create_page(attrs), to: PageManager
+  defdelegate delete_page(page), to: PageManager
+  defdelegate get_page_by_slug!(slug), to: PageManager
+  defdelegate get_page!(id), to: PageManager
+  defdelegate list_pages, to: PageManager
+  defdelegate update_page(page, attrs), to: PageManager
 end
