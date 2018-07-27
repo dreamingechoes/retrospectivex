5c3e1b1a951044de322068198725fe8d1abd7ef6
dreamingechoes <hi@dreamingecho.es>
2018-07-27 10:17:55 +0200

    Add current_admin_token method in layout_view

--- lib/retrospectivex_web/views/layout_view.ex
+++ lib/retrospectivex_web/views/layout_view.ex
@@ -1,3 +1,13 @@
 defmodule RetrospectivexWeb.LayoutView do
   use RetrospectivexWeb, :view
+
+  alias RetrospectivexWeb.FranktChannel
+
+  def current_admin_token(conn) do
+    Phoenix.Token.sign(
+      conn,
+      "administrator socket",
+      conn.assigns.current_admin.id
+    )
+  end
 end
