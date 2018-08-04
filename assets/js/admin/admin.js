fd92fc1408467167bcd132e7f6e73cbab84c6d49
dreamingechoes <hi@dreamingecho.es>
2018-08-04 18:29:52 +0200

    Add refactor to markdown textarea for page form

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -29,8 +29,15 @@ import "bootstrap/dist/js/bootstrap"
 import CodeMirror from "codemirror/lib/codemirror"
 import "codemirror/mode/markdown/markdown"
 
-CodeMirror.fromTextArea(document.getElementsByClassName("code")[0], {
+CodeMirror.fromTextArea(document.getElementsByClassName("markdown")[0], {
   lineNumbers: true,
   styleActiveLine: true,
-  matchBrackets: true
+  matchBrackets: true,
+  mode: {
+    name: "markdown",
+    highlightFormatting: true
+  },
+  lineWrapping: true,
+  tabSize: 2,
+  theme: "3024-day"
 });
