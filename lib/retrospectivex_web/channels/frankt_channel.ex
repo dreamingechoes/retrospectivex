8554ccf6727cfc71a6d4c5fe8578cdb43610515f
dreamingechoes <hi@dreamingecho.es>
2018-08-10 19:21:40 +0200

    WIP: Add refactor to user_socket and frankt_channel modules

--- lib/retrospectivex_web/channels/frankt_channel.ex
+++ lib/retrospectivex_web/channels/frankt_channel.ex
@@ -2,18 +2,11 @@ defmodule RetrospectivexWeb.FranktChannel do
   use RetrospectivexWeb, :channel
   use Frankt
 
-  def join("frankt:" <> _administrator_hash, _params, socket), do: {:ok, socket}
+  def join("frankt", _params, socket), do: {:ok, socket}
 
   def handlers do
-    %{}
-  end
-
-  def topic_name(administrator_id) do
-    client_hash =
-      :md5
-      |> :crypto.hash("#{administrator_id}")
-      |> Base.encode16()
-
-    "frankt:#{client_hash}"
+    %{
+      "board" => RetrospectivexWeb.Frankt.Retrospectives.Board
+    }
   end
 end
--- lib/retrospectivex_web/channels/user_socket.ex
+++ lib/retrospectivex_web/channels/user_socket.ex
@@ -1,60 +1,13 @@
 defmodule RetrospectivexWeb.UserSocket do
   use Phoenix.Socket
 
-  alias Retrospectivex.Accounts
-  alias Retrospectivex.Accounts.Schemas.Administrator
-
-  # Two weeks in seconds
-  @max_age 1_209_600
-
   ## Channels
-  channel("frankt:*", RetrospectivexWeb.FranktChannel)
+  channel("frankt", RetrospectivexWeb.FranktChannel)
 
   ## Transports
   transport(:websocket, Phoenix.Transports.WebSocket)
-  # transport :longpoll, Phoenix.Transports.LongPoll
 
-  # Socket params are passed from the client and can
-  # be used to verify and authenticate a user. After
-  # verification, you can put default assigns into
-  # the socket that will be set for all channels, ie
-  #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
-  #
-  # To deny connection, return `:error`.
-  #
-  # See `Phoenix.Token` documentation for examples in
-  # performing token verification on connect.
-  def connect(%{"administrator_token" => administrator_token}, socket) do
-    with {:ok, administrator_id} <-
-           Phoenix.Token.verify(
-             socket,
-             "administrator socket",
-             administrator_token,
-             max_age: @max_age
-           ),
-         %Administrator{} = current_administrator <-
-           Accounts.get_administrator!(administrator_id) do
-      socket =
-        socket
-        |> assign(:current_admin, current_administrator)
-        |> assign(:conn, %Plug.Conn{})
+  def connect(_params, socket), do: {:ok, socket}
 
-      {:ok, assign(socket, :conn, %Plug.Conn{assigns: socket.assigns})}
-    else
-      _ -> :error
-    end
-  end
-
-  # Socket id's are topics that allow you to identify all sockets for a given user:
-  #
-  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
-  #
-  # Would allow you to broadcast a "disconnect" event and terminate
-  # all active sockets and channels for a given user:
-  #
-  #     RetrospectivexWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
-  #
-  # Returning `nil` makes this socket anonymous.
   def id(_socket), do: nil
 end
