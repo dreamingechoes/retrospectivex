223520659d6e4d092878abd56faacc5040c5303c
dreamingechoes <hi@dreamingecho.es>
2018-08-13 19:27:52 +0200

    Add changes on styles and structure for board and card templates

--- lib/retrospectivex_web/views/board/board_view.ex
+++ lib/retrospectivex_web/views/board/board_view.ex
@@ -2,10 +2,4 @@ defmodule RetrospectivexWeb.BoardView do
   use RetrospectivexWeb, :view
 
   alias RetrospectivexWeb.Board.Component.ShowView, as: ShowComponentView
-
-  def options_for_status do
-    BoardStatusEnum.__enum_map__()
-    |> Keyword.keys()
-    |> Enum.sort()
-  end
 end
--- lib/retrospectivex_web/views/board/component/show_view.ex
+++ lib/retrospectivex_web/views/board/component/show_view.ex
@@ -1,6 +1,10 @@
 defmodule RetrospectivexWeb.Board.Component.ShowView do
   use RetrospectivexWeb, :view
 
+  def format_date(date) do
+    Enum.join([date.year, date.month, date.day], "/")
+  end
+
   def what_can_be_improved(cards) do
     Enum.filter(cards, &(&1.kind == :what_can_be_improved))
   end
