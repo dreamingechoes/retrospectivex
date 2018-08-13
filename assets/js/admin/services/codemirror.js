62e9b9fdc7f79351b8ce991150031d9c7c73546c
dreamingechoes <hi@dreamingecho.es>
2018-08-13 19:22:35 +0200

    Set default theme for CodeMirror in admin templates

--- assets/js/admin/services/codemirror.js
+++ assets/js/admin/services/codemirror.js
@@ -5,13 +5,11 @@ if (document.getElementsByClassName("markdown").length > 0) {
   CodeMirror.fromTextArea(document.getElementsByClassName("markdown")[0], {
     lineNumbers: true,
     styleActiveLine: true,
-    matchBrackets: true,
     mode: {
       name: "markdown",
       highlightFormatting: true
     },
-    lineWrapping: true,
     tabSize: 2,
-    theme: "3024-day"
+    theme: "default"
   });
 }
