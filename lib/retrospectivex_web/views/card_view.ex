b14758fbd05bfe841c171c69ffaf6bbc3e90f376
Ivan Gonzalez <hi@dreamingecho.es>
2018-08-15 11:57:18 +0200

    Add title_card_form method

--- lib/retrospectivex_web/views/card_view.ex
+++ lib/retrospectivex_web/views/card_view.ex
@@ -1,3 +1,6 @@
 defmodule RetrospectivexWeb.CardView do
   use RetrospectivexWeb, :view
+
+  def title_card_form(%{data: %{id: nil}}), do: gettext("Add New Card")
+  def title_card_form(%{data: %{id: _}}), do: gettext("Edit Card")
 end
