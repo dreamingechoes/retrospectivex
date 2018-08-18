e4c9c03a33c85c9918bf69093e3584ed678ef01c
dreamingechoes <hi@dreamingecho.es>
2018-08-18 20:47:53 +0200

    WIP: Add delete action for boards

--- lib/retrospectivex_web/frankt/retrospectives/board.ex
+++ lib/retrospectivex_web/frankt/retrospectives/board.ex
@@ -2,10 +2,34 @@ defmodule RetrospectivexWeb.Frankt.Retrospectives.Board do
   use RetrospectivexWeb, :frankt
 
   alias Retrospectivex.Retrospectives
-  alias Retrospectivex.Retrospectives.Schemas.Card
+  alias Retrospectivex.Retrospectives.Schemas.Board
   alias RetrospectivexWeb.Board.Component.ShowView, as: ShowComponentView
-  alias RetrospectivexWeb.CardView
+  alias RetrospectivexWeb.BoardView
 
-  def filter_cards(%{"board_filters" => board_filters}, socket) do
+  def filter_boards(%{"board_filters" => board_filters}, socket) do
+  end
+
+  def delete_modal(%{"board_id" => board_id}, socket) do
+    html =
+      render(
+        socket,
+        ShowComponentView,
+        "_delete_modal.html",
+        board_id: board_id
+      )
+
+    push(socket, "open_modal", %{html: html})
+  end
+
+  def delete(%{"board_id" => board_id}, socket) do
+    board = Retrospectives.get_board!(board_id)
+
+    case Retrospectives.delete_board(board) do
+      {:ok, _board} ->
+        push(socket, "close_modal", %{})
+
+      _error ->
+        nil
+    end
   end
 end
