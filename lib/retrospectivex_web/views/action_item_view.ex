a579fedfeb1dba27e3831acd782f2470a2bbfa8a
dreamingechoes <hi@dreamingecho.es>
2018-08-30 21:15:22 +0200

    Add action_item view

---
+++ lib/retrospectivex_web/views/action_item_view.ex
@@ -0,0 +1,9 @@
+defmodule RetrospectivexWeb.ActionItemView do
+  use RetrospectivexWeb, :view
+
+  def action_item_form_action(%{data: %{id: nil}}), do: "action_item:create"
+  def action_item_form_action(%{data: %{id: _}}), do: "action_item:update"
+
+  def title_action_item_form(%{data: %{id: nil}}), do: gettext("Add New Action Item")
+  def title_action_item_form(%{data: %{id: _}}), do: gettext("Edit Action Item")
+end
