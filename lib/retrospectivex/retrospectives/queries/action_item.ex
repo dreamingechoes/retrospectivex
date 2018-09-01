c0ab28640309d036e98094deb74a507933598a7e
Iván González Sáiz <hi@dreamingecho.es>
2018-09-01 19:21:15 +0200

    Add action_item queries module

---
+++ lib/retrospectivex/retrospectives/queries/action_item.ex
@@ -0,0 +1,14 @@
+defmodule Retrospectivex.Retrospectives.Queries.ActionItem do
+  import Ecto.Query, warn: false
+
+  alias Retrospectivex.Retrospectives.Schemas.ActionItem
+
+  def by_board(query, board) do
+    from(a in query,
+      join: c in assoc(a, :card),
+      join: b in assoc(c, :board),
+      where: b.id == ^board.id,
+      order_by: [asc: :inserted_at]
+    )
+  end
+end
