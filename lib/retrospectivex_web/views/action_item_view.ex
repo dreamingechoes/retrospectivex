e54ea7700be070fac172d5bfd26467f992c558e1
dreamingechoes <hi@dreamingecho.es>
2018-09-18 16:50:48 +0200

    Add mix format to action_item view

--- lib/retrospectivex_web/views/action_item_view.ex
+++ lib/retrospectivex_web/views/action_item_view.ex
@@ -4,6 +4,8 @@ defmodule RetrospectivexWeb.ActionItemView do
   def action_item_form_action(%{data: %{id: nil}}), do: "action_item:create"
   def action_item_form_action(%{data: %{id: _}}), do: "action_item:update"
 
-  def title_action_item_form(%{data: %{id: nil}}), do: gettext("Add New Action Item")
+  def title_action_item_form(%{data: %{id: nil}}),
+    do: gettext("Add New Action Item")
+
   def title_action_item_form(%{data: %{id: _}}), do: gettext("Edit Action Item")
 end
