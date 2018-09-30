c492c2e0088526331d4004e5242574269d4f3cb2
Iván González Sáiz <hi@dreamingecho.es>
2018-10-01 01:08:43 +0200

    Add changes to create teams migration

--- priv/repo/migrations/20180920135154_create_teams.exs
+++ priv/repo/migrations/20180920135154_create_teams.exs
@@ -3,11 +3,14 @@ defmodule Retrospectivex.Repo.Migrations.CreateTeams do
 
   def change do
     create table(:teams) do
-      add(:name, :string, null: false)
       add(:description, :text)
-      add(:members_list, :map)
+      add(:members_list, {:array, :string})
+      add(:name, :string, null: false)
+      add(:user_id, references(:users, on_delete: :delete_all))
 
       timestamps()
     end
+
+    create(index(:teams, [:user_id]))
   end
 end
