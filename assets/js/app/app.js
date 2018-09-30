0bcb8fb10ec7015be6a065684412e3a2563f02ba
Iván González Sáiz <hi@dreamingecho.es>
2018-10-01 01:08:06 +0200

    Add select2 package

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -7,12 +7,14 @@ import Popper from "popper.js"
 import "popper.js/dist/umd/popper"
 import "bootstrap/dist/js/bootstrap"
 import "flatpickr/dist/flatpickr"
+import "select2/dist/js/select2"
 
 // Services
 import "./services/frankt"
 import * as Accordion from "./services/accordion"
 import * as CodeMirror from "./services/codemirror"
 import * as Datepicker from "./services/datepicker"
+import * as Select2 from "./services/select2"
 
 // Init accordion plugin if needed
 Accordion.setup();
@@ -22,3 +24,6 @@ Datepicker.setup();
 
 // Init codemirror plugin if needed
 CodeMirror.activate("board_description");
+
+// Init select2 plugin if needed
+Select2.setup();
