8cff88639968d752a9f6f37dd159901ab4f56245
Iván González Sáiz <hi@dreamingecho.es>
2018-10-01 01:11:44 +0200

    WIP: Add new dashboard structure

--- lib/retrospectivex_web/controllers/dashboard_controller.ex
+++ lib/retrospectivex_web/controllers/dashboard_controller.ex
@@ -1,7 +1,11 @@
 defmodule RetrospectivexWeb.DashboardController do
   use RetrospectivexWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  alias Retrospectivex.Accounts
+
+  def index(conn = %{assigns: %{current_user: %{id: id}}}, _params) do
+    user = Accounts.get_user!(id)
+
+    render(conn, "index.html", teams: user.teams)
   end
 end
